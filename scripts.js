3568150178b9cab8b493f224a292693a1db45588
LiMoNtEpro <mr1lux2@mail.ru>
2026-02-24 19:27:10 +0300

    feat: add counter functionality

---
+++ scripts.js
@@ -0,0 +1,43 @@
+//tickets
+const ticketsBasicNum = document.querySelector('.counter-number-basic')
+const ticketsBasicAdjust = document.querySelector('.counter-plus-basic')
+console.log(ticketsBasicAdjust)
+const ticketsBasicReduce = document.querySelector('.counter-minus-basic')
+const ticketsSeniorNum = document.querySelector('.counter-number-senior')
+const ticketsSeniorAdjust = document.querySelector('.counter-plus-senior')
+const ticketsSeniorReduce = document.querySelector('.counter-minus-senior')
+const totalAmount = document.querySelector('.amount-title-num')
+let b=0, s=0, a=0;
+const bp = 120, sp = 100
+ticketsBasicAdjust.addEventListener('click', (event) =>{
+    console.log(event)
+    b++
+    a+= bp
+    ticketsBasicNum.innerHTML = b
+    totalAmount.innerHTML = a
+})
+ticketsBasicReduce.addEventListener('click', (event) =>{
+    if(b>0){
+        b--
+        a-= bp
+    }
+    ticketsBasicNum.innerHTML = b
+    totalAmount.innerHTML = a
+})
+ticketsSeniorAdjust.addEventListener('click', (event) =>{
+    s++
+    a+= sp
+    ticketsSeniorNum.innerHTML = s
+    totalAmount.innerHTML = a
+})
+ticketsSeniorReduce.addEventListener('click', (event) =>{
+    if(s>0){
+        s--
+        a-= sp
+    }
+    ticketsSeniorNum.innerHTML = s
+    totalAmount.innerHTML = a
+})
+
+
+
