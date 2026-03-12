2de2f26b9e4fbc184eaee81d75e38766c0f0d86f
LiMoNtEpro <mr1lux2@mail.ru>
2026-03-12 13:25:59 +0300

    feat: add loop to welcome slider

--- scripts.js
+++ scripts.js
@@ -1,9 +1,53 @@
 //welcome
-const slides = document.querySelectorAll('.welcome-image')
+const slideTrack = document.querySelector('.welcome-slider-track')
 const slideArrowLeft = document.querySelector('.slider-arrow-left')
 const slideArrowRight = document.querySelector('.slider-arrow-right')
 
 // slideArrowLeft.addEventListener('click', )
+function getSlides(){
+    return [...document.querySelectorAll('.welcome-image')]
+}
+function getNextSlide(){
+    const currentScroll = slideTrack.scrollLeft
+    return getSlides().find((slide)=>{
+        return slide.offsetLeft > currentScroll
+    })
+}
+function getPrevSlide(){
+    const currentScroll = slideTrack.scrollLeft
+    return getSlides().reverse().find((slide)=>{
+        return slide.offsetLeft < currentScroll
+    })
+}
+function getNextSlideWidth(){
+    const nextSlide = getNextSlide()
+    if (nextSlide){
+        return nextSlide.offsetLeft-slideTrack.scrollLeft
+    }
+    return -slideTrack.scrollLeft
+}
+function getPrevSlideWidth(){
+    const prevSlide = getPrevSlide()
+    if (prevSlide){
+        return prevSlide.offsetLeft-slideTrack.scrollLeft
+    }
+    return slideTrack.scrollWidth
+}
+function scrollByAmount(amount){
+    slideTrack.scrollBy({left: amount, behavior: "smooth"})
+}
+function scrollToNext(){
+    scrollByAmount(getNextSlideWidth())
+}
+function scrollToPrev(){
+    scrollByAmount(getPrevSlideWidth())
+}
+slideArrowRight.addEventListener('click', ()=>{
+    scrollToNext()
+})
+slideArrowLeft.addEventListener('click', ()=>{
+    scrollToPrev()
+})
 
 
 
@@ -11,7 +55,7 @@ const slideArrowRight = document.querySelector('.slider-arrow-right')
 //tickets
 const ticketsBasicNum = document.querySelector('.counter-number-basic')
 const ticketsBasicAdjust = document.querySelector('.counter-plus-basic')
-console.log(ticketsBasicAdjust)
+// console.log(ticketsBasicAdjust)
 const ticketsBasicReduce = document.querySelector('.counter-minus-basic')
 const ticketsSeniorNum = document.querySelector('.counter-number-senior')
 const ticketsSeniorAdjust = document.querySelector('.counter-plus-senior')
@@ -20,7 +64,7 @@ const totalAmount = document.querySelector('.amount-title-num')
 let b=0, s=0, a=0;
 const bp = 120, sp = 100
 ticketsBasicAdjust.addEventListener('click', (event) =>{
-    console.log(event)
+    // console.log(event)
     b++
     a+= bp
     ticketsBasicNum.innerHTML = b
